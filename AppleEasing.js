e08cb91560a4505927da0f0192403c0467f77206
James Ide <ide@jameside.com>
2015-07-08 18:39:36 -0700

    Adjust epsilon, setting it to 1e-9 for smoother animations
    Was getting flickering at higher values.

--- AppleEasing.js
+++ AppleEasing.js
@@ -5,9 +5,11 @@
 
 let { Easing } = require('react-native');
 
+const EPSILON = 1e-9;
+
 // See Apple's "CAMediaTimingFunction Class Reference" for the Bezier control
 // points
-exports.easeIn = Easing.bezier(0.42, 0, 1, 1);
-exports.easeOut = Easing.bezier(0, 0, 0.58, 1);
-exports.easeInOut = Easing.bezier(0.42, 0, 0.58, 1);
-exports.default = Easing.bezier(0.25, 0.1, 0.25, 1);
+exports.easeIn = Easing.bezier(0.42, 0, 1, 1, EPSILON);
+exports.easeOut = Easing.bezier(0, 0, 0.58, 1, EPSILON);
+exports.easeInOut = Easing.bezier(0.42, 0, 0.58, 1, EPSILON);
+exports.default = Easing.bezier(0.25, 0.1, 0.25, 1, EPSILON);
